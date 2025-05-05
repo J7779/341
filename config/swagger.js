56a44b77709611fff25077329adccd5f9633170c
Joe <129335973+J7779@users.noreply.github.com>
2025-05-05 17:32:51 -0500

    Update swagger.js

--- config/swagger.js
+++ config/swagger.js
@@ -11,9 +11,9 @@ const options = {
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'Your API Title',
+      title: 'DarthTator',
       version: '1.0.0',
-      description: 'API Documentation for Your Service',
+      description: 'API Documentation for DarthTator',
     },
     servers: [
       {
